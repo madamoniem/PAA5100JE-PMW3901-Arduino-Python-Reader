289301351711cd07fe31210c5ea0c66fd8c30407
Mohammad Abd-Elmoniem <98095976+madamoniem@users.noreply.github.com>
2023-06-14 16:11:23 -0400

    Updated File Structure

---
+++ Python Code/arduino_reader.py
@@ -0,0 +1,17 @@
+import time
+from main import ArduinoReader
+
+
+arduino = ArduinoReader('COM3', 9600)
+
+try:
+        while True:
+                time.sleep(1)
+                total_X, total_Y = arduino.distanceSinceInit()
+                print("Total X: ", total_X, "Total Y: ", total_Y)
+                arduino.reset()
+
+except KeyboardInterrupt:
+    arduino.stop()
+
+
